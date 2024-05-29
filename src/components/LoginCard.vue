9cf363c0c4e81866d8c9838b7e04c329bc48f9b4
Zac Paulson <zpaulson001@gmail.com>
2024-05-29 11:26:10 -0700

    Create login card

---
+++ src/components/LoginCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import BaseButton from './BaseButton.vue';
+import TextInput from './TextInput.vue';
+</script>
+
+<template>
+  <div class="border-black border-2 rounded-md bg-white p-4 grid gap-2 w-96">
+    <p class="text-xl">Welcome : )</p>
+    <form class="grid gap-4" @submit.prevent="">
+      <label for="email" class="text-sm grid gap-1">Email<TextInput /></label>
+
+      <BaseButton>Submit</BaseButton>
+    </form>
+  </div>
+</template>
