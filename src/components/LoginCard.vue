ca38c3dde54187ab27e6f46a62a6aff2d292d691
Zac Paulson <zpaulson001@gmail.com>
2024-05-29 13:54:44 -0700

    Use email input component in place of text input

--- src/components/LoginCard.vue
+++ src/components/LoginCard.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import BaseButton from './BaseButton.vue';
-import TextInput from './TextInput.vue';
+import EmailInput from './EmailInput.vue';
 </script>
 
 <template>
-  <div class="border-black border-2 rounded-md bg-white p-4 grid gap-2 w-96">
+  <div class="border-black border-2 rounded-lg bg-white p-4 grid gap-2 w-96">
     <p class="text-xl">Welcome : )</p>
     <form class="grid gap-4" @submit.prevent="">
-      <label for="email" class="text-sm grid gap-1">Email<TextInput /></label>
-
+      <label for="email" class="text-sm grid gap-1">Email<EmailInput /></label>
       <BaseButton>Submit</BaseButton>
     </form>
   </div>
