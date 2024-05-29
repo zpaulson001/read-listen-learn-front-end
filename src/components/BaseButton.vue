c725b4045667409c01d3e12ac52ea8216b4fcbbc
Zac Paulson <zpaulson001@gmail.com>
2024-05-29 11:25:43 -0700

    Create button and add pop-out animati

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  variant?: string;
+}>();
+</script>
+
+<template>
+  <button
+    class="bg-red-400 text-white border-black border-2 pop-out rounded-md p-2 active:bg-green-300 active:text-black hover:-translate-x-1 hover:-translate-y-1 transition-all"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.pop-out:hover {
+  box-shadow:
+    1px 1px black,
+    2px 2px black,
+    3px 3px black,
+    4px 4px black;
+}
+
+.pop-out:active {
+  box-shadow:
+    1px 1px black,
+    2px 2px black;
+  transform: translate(-2px, -2px);
+}
+</style>
