18013becc6c245fbe67de56fdeac85c108a1f1ce
Zac Paulson <zpaulson001@gmail.com>
2024-05-29 13:55:07 -0700

    Increase rounded-ness

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,6 +1,6 @@
 <template>
   <input
     type="text"
-    class="border-2 border-black rounded-md px-3 py-2 focus:outline-none focus:border-sky-400"
+    class="border-2 border-black rounded-lg px-3 py-2 focus:outline-none focus:border-sky-400"
   />
 </template>
